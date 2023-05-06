e18668be65d252e7a2207cd14a696559e501b9a4
= <viniciusoike@gmail.com>
2023-05-06 01:56:17 -0300

    shorten code with pmap

--- R/get_bcb_realestate.R
+++ R/get_bcb_realestate.R
@@ -1,14 +1,40 @@
-#' Title
+#' Import Real Estate data from the Brazilian Central Bank
 #'
-#' @param category aaa
-#' @param cached aaa
+#' Imports and cleans real estate data published monthly by the Brazilian Central
+#' Bank. Includes credit sources, credit applications, credit operations, financed
+#' units, real estate indices.
+#'
+#' @details
+#' The Brazilian Central Bank (BCB) publishes
+#'
+#' If `category = 'all'` a tidy long `tibble` will be returned with all available
+#' data. This table can be hard to navigate since it contains several different
+#' tables within it. Each series is uniquely identified by the `series_info` column.
+#' A complete metadata of each series is available [here](https://www.bcb.gov.br/estatisticas/mercadoimobiliario) (only in Portuguese).
+#'
+#' Other choices of `category` return a tidy wide `tibble` with informative column
+#' names.
+#'
+#' @param category One of `'accounting'`, `'application'`, `'indices'`,
+#' `'sources'`, `'units'`, or `'all'` (default).
+#' @inheritParams get_secovi
 #'
 #' @return A `tibble`
 #' @export
 #'
-#' @examples
-#' # get_bcb_realestate()
-get_bcb_realestate <- function(category = "all", cached) {
+#' @examples \dontrun { if (interactive() {
+#' # Download all data in long format
+#' bcb <- get_bcb_realestate()
+#'
+#' # Get only data on financed units
+#' units <- get_bcb_realestate("units")
+#'
+#' })}
+get_bcb_realestate <- function(category = "all", cached = FALSE) {
+
+  cats <- c("accounting", "all", "application", "indices", "sources", "units")
+
+  stopifnot(category %in% cats)
 
   if (cached) {
     df <- readr::read_csv("...")
@@ -19,21 +45,60 @@ get_bcb_realestate <- function(category = "all", cached) {
   bcb <- import_bcb_realestate()
   # Clean data
   clean_bcb <- clean_bcb_realestate(bcb)
-  return(clean_bcb)
+
+  if (category == "all") {
+    return(clean_bcb)
+  }
+
+  # Auxiliary function to pivot_wider thematic tables
+  tbl_bcb_wider <- function(cat, id_cols, names_from) {
+
+    clean_bcb |>
+      dplyr::filter(category == cat, !stringr::str_detect(type, "[0-9]")) |>
+      tidyr::pivot_wider(
+        id_cols = id_cols,
+        names_from = names_from,
+        values_from = "value",
+        names_sep = "_",
+        names_repair = "universal"
+      ) |>
+      dplyr::rename_with(~stringr::str_remove(.x, "(_br)|(br$)"))
+
+  }
+
+  # Tibble with parameters to feed the tbl_bcb_wider function
+  params <- tibble::tibble(
+    category_label = c("units", "accounting", "indices", "sources", "application"),
+    cat = c("imoveis", "contabil", "indices", "fontes", "direcionamento"),
+    id_cols = c(list(c("date", "abbrev_state")), "date", "date", "date", "date"),
+    names_from = list(c("type", "v1"), c("type", "v1"), c("type", "v1"), c("type", "v1"), c("type", "v1", "v2")
+    )
+  )
+  # Use purrr::pmap to apply function
+  params <- params |>
+    dplyr::mutate(
+      tab = purrr::pmap(list(cat, id_cols, names_from), tbl_bcb_wider)
+    )
+  # Subset for specific category
+  tbl_bcb <- params |>
+    dplyr::filter(category_label == category) |>
+    tidyr::unnest(cols = tab) |>
+    dplyr::select(-cat, -id_cols, -names_from)
+
+  return(tbl_bcb)
 
 }
-
-# TO-DO: a tbl_bcb_credit_... function
 
 import_bcb_realestate <- function() {
 
   url <- "https://olinda.bcb.gov.br/olinda/servico/MercadoImobiliario/versao/v1/odata/mercadoimobiliario?$format=text/csv&$select=Data,Info,Valor"
   # Create a temporary file name
   temp_file <- tempfile(fileext = ".csv")
+  message("Downloading real estate data from the Brazilian Central Bank.")
   # Download csv file
   download.file(url, destfile = temp_file, mode = "wb", quiet = TRUE)
   # Read the data
-  df <- readr::read_csv(temp_file)
+  df <- readr::read_csv2(temp_file, col_types = "dcc")
 
 }
 
@@ -93,99 +158,85 @@ clean_bcb_realestate <- function(df) {
 
 }
 
-# try to abstract and use purrr::pmap
-tbl_bcb_wider <- function(df, category, id_cols, names_from) {
-
-  df |>
-    dplyr::filter(category == category) |>
-    tidyr::pivot_wider(
-      id_cols = id_cols,
-      names_from = names_from,
-      values_from = "value",
-      names_sep = "_"
-    ) |>
-    dplyr::rename_with(~stringr::str_remove(.x, "_br"))
-
-}
-
-to_wide_bcb_imoveis <- function(df, t) {
-
-  df |>
-    dplyr::filter(category == "imoveis") |>
-    tidyr::pivot_wider(
-      id_cols = c("date", "abbrev_state"),
-      names_from = c("type", "v1"),
-      values_from = "value",
-      names_sep = "_"
-    )
-
-}
-
-tbl_bcb_imoveis <- function(df) {
-
-  df |>
-    dplyr::filter(category == "imoveis") |>
-    tidyr::pivot_wider(
-      id_cols = c("date", "abbrev_state"),
-      names_from = c("type", "v1"),
-      values_from = "value",
-      names_sep = "_"
-    )
-
-}
-
-tbl_bcb_contabil <- function(df) {
-
-  clean_bcb |>
-    dplyr::filter(category == "contabil") |>
-    tidyr::pivot_wider(
-      id_cols = "date",
-      names_from = c("type", "v1"),
-      values_from = "value",
-      names_sep = "_"
-    )
-
-}
-
-tbl_bcb_fontes <- function(df) {
-
-  clean_bcb |>
-    dplyr::filter(category == "fontes") |>
-    tidyr::pivot_wider(
-      id_cols = "date",
-      names_from = c("type", "v1"),
-      values_from = "value",
-      names_sort = TRUE,
-    ) |>
-    dplyr::rename_with(~stringr::str_remove(.x, "_br"))
-
-}
-
-tbl_bcb_direcionamento <- function(df) {
-
-  clean_bcb |>
-    dplyr::filter(category == "direcionamento", !stringr::str_detect(type, "[0-9]")) |>
-    tidyr::pivot_wider(
-      id_cols = "date",
-      names_from = c("type", "v1", "v2"),
-      values_from = "value",
-      names_sort = TRUE,
-    ) |>
-    dplyr::rename_with(~stringr::str_remove(.x, "_br"))
-
-}
-
-tbl_bcb_indices <- function(df) {
-
-  clean_bcb |>
-    dplyr::filter(category == "indices") |>
-    dplyr::mutate(type = stringr::str_c(type, v1)) |>
-    tidyr::pivot_wider(
-      id_cols = "date",
-      names_from = "type",
-      values_from = "value",
-      names_sort = TRUE,
-    ) |>
-    dplyr::rename_with(~stringr::str_remove(.x, "br$"))
-
-}
+# tables_bcb_realestate <- function(df, cat) {
+#
+#   if (cat == "all") {
+#
+#     units = tbl_bcb_wider(df, "imoveis", c("date", "abbrev_state"), c("type", "v1"))
+#     accounting = tbl_bcb_wider(df, "contabil", "date", c("type", "v1"))
+#     sources = tbl_bcb_wider(df, "fontes", "date", c("type", "v1"))
+#     application = tbl_bcb_wider(df, "direcionamento", "date", c("type", "v1", "v2"))
+#     indices = tbl_bcb_wider(df, "indices", "date", c("type", "v1"))
+#
+#   }
+#
+# }
+#
+# tbl_bcb_imoveis <- function(df) {
+#
+#   df |>
+#     dplyr::filter(category == "imoveis") |>
+#     tidyr::pivot_wider(
+#       id_cols = c("date", "abbrev_state"),
+#       names_from = c("type", "v1"),
+#       values_from = "value",
+#       names_sep = "_"
+#     )
+#
+# }
+#
+# tbl_bcb_contabil <- function(df) {
+#
+#   df |>
+#     dplyr::filter(category == "contabil") |>
+#     tidyr::pivot_wider(
+#       id_cols = "date",
+#       names_from = c("type", "v1"),
+#       values_from = "value",
+#       names_sep = "_"
+#     )
+#
+# }
+#
+# tbl_bcb_fontes <- function(df) {
+#
+#   df |>
+#     dplyr::filter(category == "fontes") |>
+#     tidyr::pivot_wider(
+#       id_cols = "date",
+#       names_from = c("type", "v1"),
+#       values_from = "value",
+#       names_sort = TRUE,
+#     ) |>
+#     dplyr::rename_with(~stringr::str_remove(.x, "_br"))
+#
+# }
+#
+# tbl_bcb_direcionamento <- function(df) {
+#
+#   df |>
+#     dplyr::filter(category == "direcionamento", !stringr::str_detect(type, "[0-9]")) |>
+#     tidyr::pivot_wider(
+#       id_cols = "date",
+#       names_from = c("type", "v1", "v2"),
+#       values_from = "value",
+#       names_sort = TRUE,
+#     ) |>
+#     dplyr::rename_with(~stringr::str_remove(.x, "_br"))
+#
+# }
+#
+# tbl_bcb_indices <- function(df) {
+#
+#   df |>
+#     dplyr::filter(category == "indices") |>
+#     dplyr::mutate(type = stringr::str_c(type, v1)) |>
+#     tidyr::pivot_wider(
+#       id_cols = "date",
+#       names_from = "type",
+#       values_from = "value",
+#       names_sort = TRUE,
+#     ) |>
+#     dplyr::rename_with(~stringr::str_remove(.x, "br$"))
+#
+# }
